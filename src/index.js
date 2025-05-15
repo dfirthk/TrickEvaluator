085fad9b05450d80ca06d698349e8efbd5333e3f
dfirthk <dfirthk@wgu.edu>
2025-05-15 13:30:11 -0700

    Everything looks good

--- src/index.js
+++ src/index.js
@@ -9,10 +9,11 @@ if (require('electron-squirrel-startup')) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 500,
+    height: 400,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
     },
   });
 
